86198505b20b529d9a90debb4dbb5753acb7c6b0
mfcarneiro <matheus.felipe123@gmail.com>
2019-04-13 17:58:07 -0300

    Section 9 - Responsive tips and tricks
    Added some transform and transition effects on the feed page

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -1,134 +1,140 @@
-const shareImageButton = document.querySelector("#share-image-button");
-const createPostArea = document.querySelector("#create-post");
+const shareImageButton = document.querySelector('#share-image-button');
+const createPostArea = document.querySelector('#create-post');
 const closeCreatePostModalButton = document.querySelector(
-  "#close-create-post-modal-btn"
+	'#close-create-post-modal-btn'
 );
-const sharedMomentsArea = document.querySelector("#shared-moments");
+const sharedMomentsArea = document.querySelector('#shared-moments');
 
 function openCreatePostModal() {
-  createPostArea.style.display = "block";
-  if (deferredPrompt) {
-    deferredPrompt.prompt();
+  createPostArea.style.display = 'block';
 
-    deferredPrompt.userChoice.then(function(choiceResult) {
-      console.log(choiceResult.outcome);
+	setTimeout(() => {
+		createPostArea.style.transform = 'translateY(0)';
+	}, 1);
 
-      if (choiceResult.outcome === "dismissed") {
-        console.log("User cancelled installation");
-      } else {
-        console.log("User added to home screen");
-      }
-    });
+	if (deferredPrompt) {
+		deferredPrompt.prompt();
 
-    deferredPrompt = null;
-  }
+		deferredPrompt.userChoice.then(function(choiceResult) {
+			console.log(choiceResult.outcome);
 
-  // if ('serviceWorker' in navigator) {
-  //   navigator.serviceWorker.getRegistrations()
-  //     .then(function(registrations) {
-  //       for (var i = 0; i < registrations.length; i++) {
-  //         registrations[i].unregister();
-  //       }
-  //     })
-  // }
+			if (choiceResult.outcome === 'dismissed') {
+				console.log('User cancelled installation');
+			} else {
+				console.log('User added to home screen');
+			}
+		});
+
+		deferredPrompt = null;
+	}
+
+	// if ('serviceWorker' in navigator) {
+	//   navigator.serviceWorker.getRegistrations()
+	//     .then(function(registrations) {
+	//       for (var i = 0; i < registrations.length; i++) {
+	//         registrations[i].unregister();
+	//       }
+	//     })
+	// }
 }
 
 function closeCreatePostModal() {
-  createPostArea.style.display = "none";
+  createPostArea.style.transform = 'translateY(100vh)';
+  createPostArea.style.display = 'none';
 }
 
-shareImageButton.addEventListener("click", openCreatePostModal);
+shareImageButton.addEventListener('click', openCreatePostModal);
 
-closeCreatePostModalButton.addEventListener("click", closeCreatePostModal);
+closeCreatePostModalButton.addEventListener('click', closeCreatePostModal);
 
 // Currently not in use, allows to save assets in cache on demand otherwise
 function onSaveButtonClicked(event) {
-  console.log("clicked");
+	console.log('clicked');
 
-  if ("caches" in window) {
-    caches.open("user-requested").then(function(cache) {
-      cache.add("https://httpbin.org/get");
-      cache.add("/src/images/sf-boat.jpg");
-    });
-  }
+	if ('caches' in window) {
+		caches.open('user-requested').then(function(cache) {
+			cache.add('https://httpbin.org/get');
+			cache.add('/src/images/sf-boat.jpg');
+		});
+	}
 }
 
 function clearCards() {
-  while (sharedMomentsArea.hasChildNodes()) {
-    sharedMomentsArea.removeChild(sharedMomentsArea.lastChild);
-  }
+	while (sharedMomentsArea.hasChildNodes()) {
+		sharedMomentsArea.removeChild(sharedMomentsArea.lastChild);
+	}
 }
 
 function createCard(data) {
-  const cardWrapper = document.createElement("div");
-  cardWrapper.className = "shared-moment-card mdl-card mdl-shadow--2dp";
+	const cardWrapper = document.createElement('div');
+	cardWrapper.className = 'shared-moment-card mdl-card mdl-shadow--2dp';
 
-  let cardTitle = document.createElement("div");
-  cardTitle.className = "mdl-card__title";
-  cardTitle.style.backgroundImage = "url(" + data.image + ")";
-  cardTitle.style.backgroundSize = "cover";
-  cardTitle.style.height = "180px";
-  cardWrapper.appendChild(cardTitle);
+	let cardTitle = document.createElement('div');
+	cardTitle.className = 'mdl-card__title';
+	cardTitle.style.backgroundImage = 'url(' + data.image + ')';
+	cardTitle.style.backgroundSize = 'cover';
+	cardTitle.style.height = '180px';
+	cardWrapper.appendChild(cardTitle);
 
-  let cardTitleTextElement = document.createElement("h2");
-  cardTitleTextElement.style.color = "white";
-  cardTitleTextElement.className = "mdl-card__title-text";
-  cardTitleTextElement.textContent = data.title;
-  cardTitle.appendChild(cardTitleTextElement);
+	let cardTitleTextElement = document.createElement('h2');
+	cardTitleTextElement.style.color = 'white';
+	cardTitleTextElement.className = 'mdl-card__title-text';
+	cardTitleTextElement.textContent = data.title;
+	cardTitle.appendChild(cardTitleTextElement);
 
-  let cardSupportingText = document.createElement("div");
-  cardSupportingText.className = "mdl-card__supporting-text";
-  cardSupportingText.textContent = data.location;
-  cardSupportingText.style.textAlign = "center";
-  // var cardSaveButton = document.createElement('button');
-  // cardSaveButton.textContent = 'Save';
-  // cardSaveButton.addEventListener('click', onSaveButtonClicked);
-  // cardSupportingText.appendChild(cardSaveButton);
-  cardWrapper.appendChild(cardSupportingText);
-  componentHandler.upgradeElement(cardWrapper);
-  sharedMomentsArea.appendChild(cardWrapper);
+	let cardSupportingText = document.createElement('div');
+	cardSupportingText.className = 'mdl-card__supporting-text';
+	cardSupportingText.textContent = data.location;
+	cardSupportingText.style.textAlign = 'center';
+	// var cardSaveButton = document.createElement('button');
+	// cardSaveButton.textContent = 'Save';
+	// cardSaveButton.addEventListener('click', onSaveButtonClicked);
+	// cardSupportingText.appendChild(cardSaveButton);
+	cardWrapper.appendChild(cardSupportingText);
+	componentHandler.upgradeElement(cardWrapper);
+	sharedMomentsArea.appendChild(cardWrapper);
 }
 
 function updateUI(data) {
-  clearCards();
-  for (let i = 0; i < data.length; i++) {
-    createCard(data[i]);
-  }
+	clearCards();
+	for (let i = 0; i < data.length; i++) {
+		createCard(data[i]);
+	}
 }
 
-const url = "https://pwa-gram-84973.firebaseio.com/posts.json";
+const url = 'https://pwa-gram-84973.firebaseio.com/posts.json';
 let networkDataReceived = false;
 
 fetch(url)
-  .then(function(res) {
-    return res.json();
-  })
-  .then(function(data) {
-    networkDataReceived = true;
+	.then(function(res) {
+		return res.json();
+	})
+	.then(function(data) {
+		networkDataReceived = true;
 
-    const dataArray = [];
-    for (let key in data) {
-      dataArray.push(data[key]);
-    }
-    updateUI(dataArray);
-  });
+		const dataArray = [];
+		for (let key in data) {
+			dataArray.push(data[key]);
+		}
+		updateUI(dataArray);
+	});
 
-if ("caches" in window) {
-  caches
-    .match(url)
-    .then(function(response) {
-      if (response) {
-        return response.json();
-      }
-    })
-    .then(function(data) {
-      console.log("From cache", data);
-      if (!networkDataReceived) {
-        const dataArray = [];
-        for (let key in data) {
-          dataArray.push(data[key]);
-        }
-        updateUI(dataArray);
-      }
-    });
+if ('caches' in window) {
+	caches
+		.match(url)
+		.then(function(response) {
+			if (response) {
+				return response.json();
+			}
+		})
+		.then(function(data) {
+			console.log('From cache', data);
+			if (!networkDataReceived) {
+				const dataArray = [];
+				for (let key in data) {
+					dataArray.push(data[key]);
+				}
+				updateUI(dataArray);
+			}
+		});
 }
