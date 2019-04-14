13579901ffc17d5543e5195c951cb12e98dea6d4
mfcarneiro <matheus.felipe123@gmail.com>
2019-04-14 16:38:28 -0300

    Section 9 - Background sync
    **Overall:**
    - Implemented the _background sync_ feature when a new post was added
    - Created a Firebase _functions_ to update the post list
    - Started to use Firebase hosting on app

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,7 @@
-const CACHE_STATIC_NAME = 'static-v1';
+importScripts('/src/js/idb.js');
+importScripts('/src/js/utility.js');
+
+const CACHE_STATIC_NAME = 'static-v24';
 const CACHE_DYNAMIC_NAME = 'dynamic-v2';
 const STATIC_FILES = [
 	'/',
@@ -6,6 +9,9 @@ const STATIC_FILES = [
 	'/offline.html',
 	'/src/js/app.js',
 	'/src/js/feed.js',
+	'/src/js/idb.js',
+	'/src/js/promise.js',
+	'/src/js/fetch.js',
 	'/src/js/material.min.js',
 	'/src/css/app.css',
 	'/src/css/feed.css',
@@ -15,95 +21,80 @@ const STATIC_FILES = [
 	'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
 ];
 
-// function trimCache(cacheName, maxItems) {
-//   caches.open(cacheName)
-//     .then(function (cache) {
-//       return cache.keys()
-//         .then(function (keys) {
-//           if (keys.length > maxItems) {
-//             cache.delete(keys[0])
-//               .then(trimCache(cacheName, maxItems));
-//           }
-//         });
-//     })
-// }
-self.addEventListener('install', event => {
+self.addEventListener('install', function(event) {
 	console.log('[Service Worker] Installing Service Worker ...', event);
 	event.waitUntil(
-		caches.open(CACHE_STATIC_NAME).then(cache => {
-			console.log('[Service Worker] Precaching App Shell');
+		caches.open(CACHE_STATIC_NAME).then(function(cache) {
+			console.log('[Service Worker] Pre-caching App Shell');
 			cache.addAll(STATIC_FILES);
 		})
 	);
 });
 
-self.addEventListener('activate', event => {
+self.addEventListener('activate', function(event) {
 	console.log('[Service Worker] Activating Service Worker ....', event);
 	event.waitUntil(
-		caches.keys().then(keyList => {
+		caches.keys().then(function(keyList) {
 			return Promise.all(
-				keyList.map(key => {
+				keyList.map(function(key) {
 					if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
 						console.log('[Service Worker] Removing old cache.', key);
-
 						return caches.delete(key);
 					}
 				})
 			);
 		})
 	);
-
 	return self.clients.claim();
 });
 
 function isInArray(string, array) {
-	let cachePath;
-
+	var cachePath;
 	if (string.indexOf(self.origin) === 0) {
 		// request targets domain where we serve the page from (i.e. NOT a CDN)
+		console.log('matched ', string);
 		cachePath = string.substring(self.origin.length); // take the part of the URL AFTER the domain (e.g. after localhost:8080)
 	} else {
 		cachePath = string; // store the full request (for CDNs)
 	}
-
 	return array.indexOf(cachePath) > -1;
 }
-self.addEventListener('fetch', event => {
-	const url = 'https://pwa-gram-84973.firebaseio.com/posts';
 
+self.addEventListener('fetch', function(event) {
+	var url = 'https://pwa-gram-84973.firebaseio.com/posts';
 	if (event.request.url.indexOf(url) > -1) {
 		event.respondWith(
-			fetch(event.request).then(response => {
-				const clonedResponse = response.clone();
+			fetch(event.request).then(function(res) {
+				var clonedRes = res.clone();
 				clearAllData('posts')
-					.then(() => {
-						return clonedResponse.json();
+					.then(function() {
+						return clonedRes.json();
 					})
-					.then(data => {
-						for (let key in data) {
-							writeData(data[key]);
+					.then(function(data) {
+						for (var key in data) {
+							writeData('posts', data[key]);
 						}
 					});
-				return response;
+				return res;
 			})
 		);
 	} else if (isInArray(event.request.url, STATIC_FILES)) {
 		event.respondWith(caches.match(event.request));
 	} else {
 		event.respondWith(
-			caches.match(event.request).then(response => {
+			caches.match(event.request).then(function(response) {
 				if (response) {
 					return response;
 				} else {
 					return fetch(event.request)
-						.then(res => {
-							return caches.open(CACHE_DYNAMIC_NAME).then(cache => {
+						.then(function(res) {
+							return caches.open(CACHE_DYNAMIC_NAME).then(function(cache) {
 								cache.put(event.request.url, res.clone());
 								return res;
 							});
 						})
-						.catch(err => {
-							return caches.open(CACHE_STATIC_NAME).then(cache => {
+						.catch(function(err) {
+							return caches.open(CACHE_STATIC_NAME).then(function(cache) {
 								if (event.request.headers.get('accept').includes('text/html')) {
 									return cache.match('/offline.html');
 								}
@@ -114,7 +105,47 @@ self.addEventListener('fetch', event => {
 		);
 	}
 });
-// NOTE: Another strategies
+
+self.addEventListener('sync', function(event) {
+	console.log('[Service Worker] Background syncing', event);
+	if (event.tag === 'sync-new-posts') {
+		console.log('[Service Worker] Syncing new Posts');
+		event.waitUntil(
+			readAllData('sync-posts').then(function(data) {
+				for (let dt of data) {
+					fetch(
+						'https://us-central1-pwa-gram-84973.cloudfunctions.net/storePostData',
+						{
+							method: 'POST',
+							headers: {
+								'Content-Type': 'application/json',
+								Accept: 'application/json',
+							},
+							body: JSON.stringify({
+								id: dt.id,
+								title: dt.title,
+								location: dt.location,
+								image:
+									'https://firebasestorage.googleapis.com/v0/b/pwa-gram-84973.appspot.com/o/sf-boat.jpg?alt=media&token=3a6e141c-ece2-4236-90ba-2dd5cedc0b83',
+							}),
+						}
+					)
+						.then(function(res) {
+							console.log('Sent data', res);
+							if (res.ok) {
+								res.json().then(function(resData) {
+									deleteItemFromData('sync-posts', resData.id);
+								});
+							}
+						})
+						.catch(function(err) {
+							console.log('Error while sending data', err);
+						});
+				}
+			})
+		);
+	}
+});
 
 // self.addEventListener('fetch', function(event) {
 //   event.respondWith(
